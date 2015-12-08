81db6b3ba1d8bcb68015f1aac9321f5bd12441ac
klucznik <user@pc7.home>
2015-12-09 00:56:02 +0100

    uppercase consts

--- src/StatusType.php
+++ src/StatusType.php
@@ -1,15 +1,15 @@
 <?php namespace SagePay;
 
 	abstract class StatusType {
-		const Ok = 'OK';
-		const NotAuthed = 'NOTAUTHED';
+		const OK = 'OK';
+		const NOTAUTHED = 'NOTAUTHED';
 
-		const Rejected = 'REJECTED';
-		const Invalid = 'INVALID';
-		const Malformed = 'MALFORMED';
-		const Error = 'ERROR';
+		const REJECTED = 'REJECTED';
+		const INVALID = 'INVALID';
+		const MALFORMED = 'MALFORMED';
+		const ERROR = 'ERROR';
 
-		const ThreeDStart = '3DAUTH';
-		const ThreeDAuthenticated = 'AUTHENTICATED'; //The 3D-Secure checks were performed successfully and the card details secured at Sage Pay.
-		const ThreeDRegistered = 'REGISTERED'; //3D-Secure checks failed or were not performed, but the card details are still secured at Sage Pay.
+		const THREEDSTART = '3DAUTH';
+		const THREEDAUTHENTICATED = 'AUTHENTICATED'; //The 3D-Secure checks were performed successfully and the card details secured at Sage Pay.
+		const THREEDREGISTERED = 'REGISTERED'; //3D-Secure checks failed or were not performed, but the card details are still secured at Sage Pay.
 	}
